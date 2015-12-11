a9a9667360af040758866180440fa43c17388b1c
Dennis Loska <lod@arktis-gmbh.de>
2015-12-11 13:22:12 +0100

    Lucene-Grundstruktur

--- src/main/java/de/arktis/javafx/contact/SearchEngine/LucenePersonEngine.java
+++ src/main/java/de/arktis/javafx/contact/SearchEngine/LucenePersonEngine.java
@@ -1,5 +1,19 @@
 package de.arktis.javafx.contact.SearchEngine;
 
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.IOException;
+
 /**
  * Created by Pati on 11.12.2015.
  */
@@ -7,6 +21,5 @@ public class LucenePersonEngine {
 
     public LucenePersonEngine(){
     }
-
 
 }
